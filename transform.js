9423f75b849a702092c82f1d67281506184e2c3a
xukai <xukai@ai2-jp.com>
2019-08-21 11:16:19 +0900

    move to docs

--- transform.js
+++ transform.js
@@ -5,11 +5,11 @@ console.log(`transform ${distFile} ...`);
 fs.readFile(distFile, (err, data) => {
     let newFile = data.toString();
     [
-        {src: '/js/', dest: 'https://raw.githubusercontent.com/hydraslay/vue-particle/master/dist/js/'},
-        {src: '/css/', dest: 'https://raw.githubusercontent.com/hydraslay/vue-particle/master/dist/css/'},
-        {src: '/img/', dest: 'https://raw.githubusercontent.com/hydraslay/vue-particle/master/dist/img/'},
-        {src: '/favicon.ico', dest: 'https://raw.githubusercontent.com/hydraslay/vue-particle/master/dist/favicon.ico'},
-        {src: '/manifest.json', dest: 'https://raw.githubusercontent.com/hydraslay/vue-particle/master/dist/manifest.json'},
+        {src: '/js/', dest: '/doc/js/'},
+        {src: '/css/', dest: '/doc/css/'},
+        {src: '/img/', dest: '/doc/img/'},
+        {src: '/favicon.ico', dest: '/doc/favicon.ico'},
+        {src: '/manifest.json', dest: '/doc/manifest.json'},
     ].forEach(item => {
         newFile = newFile.split(item.src).join(item.dest)
     })
